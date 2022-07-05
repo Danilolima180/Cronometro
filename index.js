ec65f873b4f221534998d8aaa3337f4c1eae211b
Danilo <danilolima180@gmail.com>
2022-07-04 22:48:08 -0300

    criando variáveis

---
+++ index.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+
+  var seconds = 00;
+  
+  var tens = 00;
+  
+  var appendTens = 
+  document.getElementById("tens");
+  
+  var appendSecond =
+  document.getElementsById('seconds');
+ 
+  var buttonStart =
+  document.getElementById("button-start");
+  
+  var buttonStop = 
+  document.getElementById("button-stop");
+  
+  var buttonReset = document.getElementById("button-reset");
+
+  var Interval ; 
+  
+}
