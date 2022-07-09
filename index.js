eb835f6a6a445ef2745e05c2c9f7f8f59811c73a
Danilo <danilolima180@gmail.com>
2022-07-09 14:11:57 -0300

    Codando o JS

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 window.onload = function () {
 
   var seconds = 00;
-  
   var tens = 00;
-  
-  var appendTens = 
-  document.getElementById("tens");
-  
-  var appendSecond =
-  document.getElementsById('seconds');
- 
-  var buttonStart =
-  document.getElementById("button-start");
-  
-  var buttonStop = 
-  document.getElementById("button-stop");
-  
+  var appendTens = document.getElementById("tens");
+  var appendSecond = document.getElementsById('seconds');
+  var buttonStart = document.getElementById("button-start");
+  var buttonStop =  document.getElementById("button-stop");
   var buttonReset = document.getElementById("button-reset");
-
   var Interval ; 
-  
+
+  buttonStart.onclick = function(){
+
+    clearInterval(Interval);
+    Interval = setInterval(startTimer, 10);
+  }
+
+    buttonStop.onclick = function(){
+      clearInterval(interval);
+      tens = "00";
+      seconds = "00";
+      appendTens.innerHTML = tens;
+      appendSecond.innerHtml = seconds;
+    }
+    
+    function startTimer (){
+      tens++;
+    }
+
 }
